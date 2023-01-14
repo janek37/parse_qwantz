e8c289ace3b1840edcdea74617ce6ca7a54c7709
Jan Szejko <janek37@gmail.com>
2023-01-14 15:45:05 +0100

    exclude borders from extracted panels

--- prepare_panels.sh
+++ prepare_panels.sh
@@ -7,11 +7,11 @@ tempdir=$(mktemp -d)
 masked="$tempdir/masked.png"
 convert "$filename" img/mask.png -compose plus -composite "$masked"
 
-convert "$masked" -crop 244x243+0+0 "$tempdir/panel1.png"
-convert "$masked" -crop 130x243+244+0 "$tempdir/panel2.png"
-convert "$masked" -crop 361x243+374+0 "$tempdir/panel3.png"
-convert "$masked" -crop 195x245+0+243 "$tempdir/panel4.png"
-convert "$masked" -crop 295x245+195+243 "$tempdir/panel5.png"
-convert "$masked" -crop 242x245+493+243 "$tempdir/panel6.png"
+convert "$masked" -crop 239x239+3+2 "$tempdir/panel1.png"
+convert "$masked" -crop 126x239+246+2 "$tempdir/panel2.png"
+convert "$masked" -crop 358x239+375+2 "$tempdir/panel3.png"
+convert "$masked" -crop 190x239+3+244 "$tempdir/panel4.png"
+convert "$masked" -crop 295x239+196+244 "$tempdir/panel5.png"
+convert "$masked" -crop 239x239+494+244 "$tempdir/panel6.png"
 
 echo "$tempdir"
