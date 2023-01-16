98acee4610e8a928758d6e43cf77cf53b659a234
Jan Szejko <janek37@gmail.com>
2023-01-16 08:02:21 +0100

    add tiny font

--- character_shapes.py
+++ character_shapes.py
@@ -9,6 +9,7 @@ PRINTABLE = string.printable.strip()
 
 REGULAR_SHAPE_FILE = 'img/regular.png'
 REGULAR11_SHAPE_FILE = 'img/regular11.png'
+REGULAR8_SHAPE_FILE = 'img/regular8.png'
 
 
 @dataclass
@@ -91,6 +92,8 @@ def get_bitmask(pixel: Pixel, image: SimpleImage, width: int, height: int) -> in
 
 REGULAR_FONT = Font('Regular', *get_regular_shapes(REGULAR_SHAPE_FILE, shifted_variants={',': 1}))
 BOLD_FONT = Font('Bold', *get_bold_shapes(REGULAR_FONT))
-REGULAR11_FONT = Font('Small', *get_regular_shapes(REGULAR11_SHAPE_FILE))
+SMALL_FONT = Font('Small', *get_regular_shapes(REGULAR11_SHAPE_FILE))
+TINY_FONT = Font('Tiny', *get_regular_shapes(REGULAR8_SHAPE_FILE))
 
-ALL_FONTS = [REGULAR_FONT, BOLD_FONT, REGULAR11_FONT]
+
+ALL_FONTS = [REGULAR_FONT, BOLD_FONT, SMALL_FONT, TINY_FONT]
