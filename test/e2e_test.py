6dca91bd991242122a7032c7db69e65c7922bb30
Jan Szejko <janek37@gmail.com>
2023-01-22 23:45:12 +0100

    add name parameter in tests to make results clearer

--- test/e2e_test.py
+++ test/e2e_test.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 import pytest
@@ -9,8 +8,8 @@ INPUT_FILE_DIR = Path('test/comics')
 EXPECTED_OUTPUT_DIR = Path('test/expected_outputs')
 
 
-@pytest.mark.parametrize('input_file_path', sorted(INPUT_FILE_DIR.iterdir()))
-def test_output(capsys, input_file_path: Path):
+@pytest.mark.parametrize(['name', 'input_file_path'], [(path.stem, path) for path in sorted(INPUT_FILE_DIR.iterdir())])
+def test_output(capsys, name: str, input_file_path: Path):
     main(str(input_file_path))
     captured = capsys.readouterr()
     expected_output = open(EXPECTED_OUTPUT_DIR / (input_file_path.stem + '.txt')).read()
