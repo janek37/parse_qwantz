b35b3aff53a868c57b1eb1fe735cc1ea40c7b47a
Jan Szejko <janek37@gmail.com>
2023-01-24 18:42:20 +0100

    simplify thought detection a little

--- detect_thought.py
+++ detect_thought.py
@@ -13,13 +13,13 @@ def get_thought(pixel: Pixel, image: SimpleImage) -> tuple[Box, list[Pixel]] | N
     pixels: set[Pixel] = set(orig_pixels)
     for x, y in orig_pixels:
         pixels.update({Pixel(x + 1, y), Pixel(x + 2, y)})
-    x_range = range(box.left + 1, box.right)
-    y_range = range(box.top + 1, box.bottom)
+    x_range = range(box.left + 1, box.right - 1)
+    y_range = range(box.top + 1, box.bottom - 1)
     outside = set()
     outside.update(Pixel(box.left, y) for y in y_range)
     outside.update(Pixel(box.right, y) for y in y_range)
-    outside.update((Pixel(x, box.top) for x in x_range))
-    outside.update((Pixel(x, box.bottom) for x in x_range))
+    outside.update((Pixel(x, box.top - 1) for x in x_range))
+    outside.update((Pixel(x, box.bottom - 1) for x in x_range))
     non_empty_interior = False
     for x, y in product(x_range, y_range):
         if Pixel(x, y) in pixels:
@@ -28,13 +28,11 @@ def get_thought(pixel: Pixel, image: SimpleImage) -> tuple[Box, list[Pixel]] | N
         to_visit = deque([Pixel(x, y)])
         while to_visit:
             current = to_visit.popleft()
-            if current in visited or current in pixels:
+            if current in visited or current in pixels or not box.includes(current):
                 continue
             if current in outside:
                 outside.update(visited)
                 break
-            if not box.includes(current):
-                continue
             visited.add(current)
             to_visit.extend(get_adjacent_pixels(current))
         else:
