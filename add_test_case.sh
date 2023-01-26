4298ba9103e9e68adeb1a82138f44132c3400ad9
Jan Szejko <janek37@gmail.com>
2023-01-26 18:33:40 +0100

    move the local path to an env variable

--- add_test_case.sh
+++ add_test_case.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 number="$1"
-COMIC_DIR=~/media/teksty/różności/qwantz/comics
 
 stdout_file=test/expected_outputs/$number.txt
 python parse_qwantz.py $COMIC_DIR/"${number}"* > "$stdout_file"
