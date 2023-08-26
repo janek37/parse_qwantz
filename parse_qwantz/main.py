932ffe2d5402bc4cfbe46a721dac6b6f317295ae
Jan Szejko <janek37@gmail.com>
2023-08-26 10:05:49 +0200

    Fix #2935

--- parse_qwantz/main.py
+++ parse_qwantz/main.py
@@ -235,7 +235,7 @@ def match_above_or_below(unmatched_blocks: list[TextBlock], block_matches: dict[
 
 
 def handle_god_and_devil(block: TextBlock, is_off_panel: bool) -> Character | None:
-    if not block.content().isupper():
+    if any(char.islower() for char in block.content()):
         return None
     if block.color == Color.RED and is_off_panel and block.is_bold:
         return Character.from_name('Devil')
