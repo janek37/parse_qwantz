4337a21c82801e3d9d1b27b5fd6f3e02b3e895ed
Jan Szejko <janek37@gmail.com>
2024-02-05 18:06:20 +0100

    Add override for #4153

--- add_panel_override.py
+++ add_panel_override.py
@@ -11,12 +11,12 @@ from parse_qwantz.parser import parse_qwantz
 app = typer.Typer()
 
 
-def add_panel_override(image_path: Path, panel_no: list[int]) -> None:
+def add_panel_override(image_path: Path, comic_id: int, panel_no: list[int]) -> None:
     overrides = json.load(open('parse_qwantz/data/panel_overrides.json'))
     image = Image.open(image_path)
     md5 = hashlib.md5(image.tobytes()).hexdigest()
     if md5 not in overrides:
-        comic_id, file_name = image_path.name.split(' - ')
+        file_name = image_path.name
         overrides[md5] = {"comic_id": int(comic_id), "file_name": file_name, "panels": {}}
     panels = list(parse_qwantz(image))
     for n in panel_no:
